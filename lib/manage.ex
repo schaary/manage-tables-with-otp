319f307f808404b6c476bdb3004f49f73a68960a
Michael Schaarschmidt <michael@schaaryworks.com>
2017-10-15 11:06:42 +0200

    initial commit

---
+++ lib/manage.ex
@@ -0,0 +1,18 @@
+defmodule Manage do
+  @moduledoc """
+  Documentation for Manage.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Manage.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
